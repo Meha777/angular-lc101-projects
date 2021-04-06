2ff534e889b329cca38cbc1851e7297461b179ca
Meha777 <mafework28@gmail.com>
2021-04-06 01:00:58 -0500

    finished part 5- fetching satellites

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Satellite} from './satellite' 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
-}
+  sourceList: Satellite[];
+
+  constructor() {
+    this.sourceList = [];
+    const satellitesUrl = '../assets/satellites.json';
+    window.fetch(satellitesUrl).then(function(response) {
+
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+
+          for (let i = 0; i < fetchedSatellites.length; i++){
+            let satellite = new Satellite(fetchedSatellites[i].name, 
+              fetchedSatellites[i].type, fetchedSatellites[i].launchDate, 
+              fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+              this.sourceList.push(satellite)
+
+              //  for (let i = 0; i < fetchedSatellites.length; i++)
+              //   if (satellite.type === 'Space Debris') {
+              //     ShouldShowWarning();
+              //   }
+               
+
+          };
+          // TODO: loop over satellites
+          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+ 
+
+      }.bind(this));
+
+    }.bind(this));
+    
+    };
+ };
+
+
+
+
