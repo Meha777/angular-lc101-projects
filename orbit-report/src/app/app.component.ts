c28cb93316aa0c83fc80759e9c5799cb08f1ccb9
Meha777 <mafework28@gmail.com>
2021-04-08 12:40:08 -0500

    worked on part 8

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Satellite} from './satellite' 
+import {Satellite} from './Satellite' 
 
 @Component({
   selector: 'app-root',
@@ -25,15 +25,15 @@ export class AppComponent {
             let satellite = new Satellite(fetchedSatellites[i].name, 
               fetchedSatellites[i].type, fetchedSatellites[i].launchDate, 
               fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-              this.sourceList.push(satellite)
-              this.displayList = this.sourceList.slice(0);
+              this.sourceList.push(satellite);
+              
           };
             
           // TODO: loop over satellites
           // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
           
-
+          this.displayList = this.sourceList.slice(0);
       }.bind(this));
 
     }.bind(this));
@@ -41,19 +41,18 @@ export class AppComponent {
   };
 
   search(searchTerm: string): void {
-    let matchingSatellites: Satellite[] = [];
+    let matchingSatellites: Satellite[];
     searchTerm = searchTerm.toLowerCase();
     for(let i=0; i < this.sourceList.length; i++) {
        let name = this.sourceList[i].name.toLowerCase();
        if (name.indexOf(searchTerm) >= 0) {
           matchingSatellites.push(this.sourceList[i]);
-          // this.displayList = matchingSatellites;
-          // this.displayList = this.sourceList.slice(0);
+          
        }
     } 
     // assign this.displayList to be the array of matching satellites
     // this will cause Angular to re-make the table, but now only containing matches
-    //  this.displayList = matchingSatellites;
+    this.displayList = matchingSatellites;
     // this.displayList = this.sourceList
     
  }
